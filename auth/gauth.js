b63ee8b41d07b07c12596d8312eb4e2c5c2cf532
69series <gamignite120@gmail.com>
2026-04-24 09:34:10 +0530

    corrected callback URL

--- auth/gauth.js
+++ auth/gauth.js
@@ -9,7 +9,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.callbackURL,
+    callbackURL: process.env.CALLBACK_URL,
     passReqToCallback   : true
   },
   async function(request, accessToken, refreshToken, profile, done) {
